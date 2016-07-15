78171f6dd17a647f0d8be4a30beb88133e376400
Dabo Ross <daboross@daboross.net>
2016-07-15 00:58:07 -0700

    Implement PluginStatistics.java.
    Some of the ideas for how to do things are taken from MCStats, but all of the code itself is written from scratch.
    
    This also copies a class from https://github.com/daboross/json-serialization to do the JSON serialization.
    
    This has been tested, and is confirmed working!

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ A small still-in-testing plugin statistics library. Reports to https://dabo.guru
 
 Data is reported to https://dabo.guru, and is reported once every hour, starting one hour from server startup (servers running for under one hour will not have any data reported).
 
-Plugin statistics reports a server GUID, the plugin name, the plugin version, the number of players online on the server, and the version of the server.
+Plugin statistics reports a server UUID, the plugin name, the plugin version, the number of players online on the server, and the version of the server.
 
 
 Data Reporting
@@ -13,26 +13,26 @@ Data Reporting
 
 Example data (sent to `https://dabo.guru/statistics/v1/<plugin_name>/post`):
 
-```
+```json
 {
-    "guid": "1aa76cf6-496d-11e6-8243-5c260a7a2dbe",
+    "instance_uuid": "1aa76cf6-496d-11e6-8243-5c260a7a2dbe",
     "plugin_version": "2.1.5",
     "server_version": "1.8.8",
     "online_players": "21"
 }
 ```
 
-Note that the server GUID is generated uniquely on each server startup - it is unique to your server, but is in no way identifying. The only reason a GUID is included at all is to avoid duplicate data reporting, as plugin-statistics reports every hour, and reports are kept for 2-3 hours in the service.
+Note that the server UUID is generated uniquely on each server startup - it is unique to your server, but is in no way identifying. The only reason a UUID is included at all is to avoid duplicate data reporting, as plugin-statistics reports every hour, and reports are kept for 2-3 hours in the service.
 
-The server stores each submitted data set (guid, plugin_version, server_version and online_players) for up to 3 hours. When the plugin send another report with the same GUID, the server erases all previous data for that GUID.
+The server stores each submitted data set (instance_uuid, plugin_version, server_version and online_players) for up to 3 hours. When the plugin send another report with the same UUID, the server erases all previous data for that UUID.
 
-Every hour, on the hour, dabo.guru erases all data sets which are over 2 hours old, and then creates a "record" which aggregates the remaining data. The hourly record does not store any GUIDs, but stores the following data sets:
+Every hour, on the hour, dabo.guru erases all data sets which are over 2 hours old, and then creates a "record" which aggregates the remaining data. The hourly record does not store any UUIDs, but stores the following data sets:
 
 - Total online players for the plugin (sum of all online players on all servers)
 - Total number of servers running each plugin_version (count of each server stored per plugin_version)
 - Total number of plugins with each unique server_version and plugin_version combination (count of each server stored per (plugin_version, server_version))
 
-The data in the hourly "records" is stored indefinitely. But, as you can see, it does not contain server GUIDs, or any other server-identifying information. If your server has been offline for at least 3 hours, there will be no information stored about it.
+The data in the hourly "records" is stored indefinitely. But, as you can see, it does not contain server UUIDs, or any other server-identifying information. If your server has been offline for at least 3 hours, there will be no information stored about it.
 
 This service is similar to MCStats, but records vastly less information, and does not yet have any way to retrieve information. The only reason I've created it is to view one specific trend: server versions for each plugin version.
 
--- src/main/java/net/daboross/bukkitdev/pluginstatistics/PluginStatistics.java
+++ src/main/java/net/daboross/bukkitdev/pluginstatistics/PluginStatistics.java
@@ -1,4 +1,352 @@
 package net.daboross.bukkitdev.pluginstatistics;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.UUID;
+import java.util.logging.Level;
+import org.apache.commons.lang.Validate;
+import org.bukkit.Server;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
 public class PluginStatistics {
+
+    private static final String API_URL_FORMAT = "https://dabo.guru/statistics/v1/%s/post";
+    private static final long INTERVAL_SECONDS = 60 * 60; // Report every hour.
+    private static final Object taskLock = new Object();
+
+    private final Plugin plugin;
+    private final UUID instanceUuid;
+    private final boolean debug;
+    private int taskId;
+
+    public PluginStatistics(Plugin plugin, final boolean debug) {
+        Validate.notNull(plugin);
+        this.plugin = plugin;
+        this.debug = debug;
+        this.instanceUuid = UUID.randomUUID();
+        this.taskId = -1;
+    }
+
+    public void start() {
+        synchronized (taskLock) {
+            if (taskId != -1) {
+                return;
+            }
+            long intervalTicks = INTERVAL_SECONDS * 20;
+            taskId = new ReportRunnable().runTaskTimerAsynchronously(plugin, 200, intervalTicks).getTaskId();
+        }
+    }
+
+    public void stop() {
+        synchronized (taskLock) {
+            if (taskId == -1) {
+                return;
+            }
+            plugin.getServer().getScheduler().cancelTask(taskId);
+            taskId = -1;
+        }
+    }
+
+    public class ReportRunnable extends BukkitRunnable {
+
+        @Override
+        public void run() {
+            String pluginName = plugin.getName();
+            String pluginVersion = plugin.getDescription().getVersion();
+            String serverVersion = plugin.getServer().getVersion();
+            int onlinePlayers = 0;
+            try {
+                onlinePlayers = plugin.getServer().getOnlinePlayers().size();
+            } catch (NoSuchMethodError ex) {
+                try {
+                    Method m = Server.class.getMethod("getOnlinePlayers");
+                    onlinePlayers = ((Player[]) m.invoke(plugin.getServer())).length;
+                } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e2) {
+                    if (debug) {
+                        plugin.getLogger().log(Level.WARNING, "[statistics] Unable to get online player count.", e2);
+                    }
+                }
+            }
+
+            Map<String, Object> dataMap = new HashMap<>(4);
+            dataMap.put("instance_uuid", instanceUuid.toString());
+            dataMap.put("plugin_version", pluginVersion);
+            dataMap.put("server_version", serverVersion);
+            dataMap.put("online_players", onlinePlayers);
+
+            URL apiUrl;
+            try {
+                apiUrl = new URL(String.format(API_URL_FORMAT, URLEncoder.encode(pluginName, "UTF-8")));
+            } catch (MalformedURLException | UnsupportedEncodingException ex) {
+                if (debug) {
+                    plugin.getLogger().log(Level.WARNING, "[statistics] Failed to encode API URL.", ex);
+                }
+                return;
+            }
+
+            byte[] encodedData;
+
+            try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream()) {
+                // By using an inner try-with-resources block, the outputstreamwriter will always be flushed&closed before
+                // calling byteArrayOutpuStream.toByteArray().
+                try (OutputStreamWriter outputStreamWriter = new OutputStreamWriter(byteArrayOutputStream, "UTF-8")) {
+                    JsonSerialization.writeJsonObject(outputStreamWriter, dataMap);
+                }
+                encodedData = byteArrayOutputStream.toByteArray();
+            } catch (IOException | JsonSerialization.JsonException ex) {
+                if (debug) {
+                    plugin.getLogger().log(Level.WARNING, "[statistics] Failed to encode and compress data to submit.", ex);
+                }
+                return;
+            }
+
+            HttpURLConnection connection;
+            try {
+                connection = (HttpURLConnection) apiUrl.openConnection();
+            } catch (IOException | ClassCastException ex) {
+                if (debug) {
+                    plugin.getLogger().log(Level.WARNING, "[statistics] Failed to initiate connection.", ex);
+                }
+                return;
+            }
+            connection.addRequestProperty("Accept", "*/*");
+            connection.addRequestProperty("Content-Length", String.valueOf(encodedData.length));
+            connection.addRequestProperty("Content-Type", "application/json");
+            connection.addRequestProperty("Content-Encoding", "gzip");
+            connection.addRequestProperty("Connection", "close");
+            connection.addRequestProperty("User-Agent", "plugin-statistics/v1");
+
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+
+            int responseCode;
+
+            try {
+                connection.connect();
+
+                try (OutputStream outputStream = connection.getOutputStream()) {
+                    outputStream.write(encodedData);
+                }
+
+                responseCode = connection.getResponseCode();
+            } catch (IOException ex) {
+                if (debug) {
+                    plugin.getLogger().log(Level.WARNING, "[statistics] Failed to connect to service.", ex);
+                }
+                return;
+            }
+
+            if (debug && responseCode != 200) {
+                plugin.getLogger().log(Level.WARNING, "[statistics] Service returned non-OK response code: {0}", responseCode);
+                try (StringWriter writer = new StringWriter()) {
+                    JsonSerialization.writeJsonObject(writer, dataMap);
+                    plugin.getLogger().log(Level.INFO, "[statistics] POST data which caused this error: {0}", writer.toString());
+                } catch (JsonSerialization.JsonException | IOException ex) {
+                    plugin.getLogger().log(Level.WARNING, "[statistics] Failed to pretty-print data (to show the POST request which caused the error).", ex);
+                }
+            }
+        }
+    }
+
+    /**
+     * Class allowing for serializing Java Maps/Lists as json objects/arrays to a writer.
+     * <p>
+     * This is part of https://github.com/daboross/json-serialization, used with permission.
+     * <p>
+     * The only difference between the original class and this class is that indent/pretty-printing support has been
+     * removed.
+     *
+     * @author daboross@daboross.net (David Ross)
+     */
+    public static class JsonSerialization {
+
+        /**
+         * Writes a number to the given writer in a format valid for json.
+         *
+         * @param writer The writer to write to.
+         * @param number The number to write.
+         * @throws JsonException If number is not finite.
+         * @throws IOException   If the writer throws an IOException.
+         */
+        public static void writeNumber(Writer writer, Number number) throws JsonException, IOException {
+            if (Double.isNaN(number.doubleValue()) || Double.isInfinite(number.doubleValue())) {
+                throw new JsonException("Expected finite number, found `" + number + "`");
+            }
+            writer.write(number.toString());
+        }
+
+        /**
+         * Writes a string to the given writer as an escaped json string.
+         *
+         * @param writer The writer to write to.
+         * @param string The string to write.
+         * @throws IOException If the writer throws an IOException.
+         */
+        public static void writeString(Writer writer, String string) throws IOException {
+            writer.write('\"');
+            char previousChar = 0;
+            for (char c : string.toCharArray()) {
+                switch (c) {
+                    case '\\':
+                    case '\"':
+                        writer.write('\\');
+                        writer.write(c);
+                        break;
+                    case '/':
+                        if (previousChar == '<') {
+                            writer.write('\\');
+                        }
+                        writer.write(c);
+                        break;
+                    case '\b':
+                        writer.write("\\b");
+                        break;
+                    case '\t':
+                        writer.write("\\t");
+                        break;
+                    case '\n':
+                        writer.write("\\n");
+                        break;
+                    case '\f':
+                        writer.write("\\f");
+                        break;
+                    case '\r':
+                        writer.write("\\r");
+                        break;
+                    default:
+                        if (c < ' ' || (c >= '\u0080' && c < '\u00a0')
+                                || (c >= '\u2000' && c < '\u2100')) {
+                            writer.write("\\u");
+                            String hexStringTemp = Integer.toHexString(c);
+                            writer.write("0000", 0, 4 - hexStringTemp.length());
+                            writer.write(hexStringTemp);
+                        } else {
+                            writer.write(c);
+                        }
+                }
+                previousChar = c;
+            }
+            writer.write('\"');
+        }
+
+        /**
+         * Writes a json object string from the given Map to the given writer. Please note that this assumes that no
+         * data structures are cyclical, and that all iterables are finite.
+         *
+         * @param writer The writer to write to.
+         * @param values The value map. All keys will be proccessed with String.valueOf(), and values will be formatted
+         *               depending on type. Of values taken from the map: All Iterables will be treated as json arrays,
+         *               and all maps will be formatted as json maps.
+         * @throws JsonException If a value of an unknown type is found.
+         * @throws IOException   If the writer throws an IOException.
+         */
+        public static Writer writeJsonObject(Writer writer, Map<?, ?> values) throws JsonException, IOException {
+            boolean commanate = false;
+            final int length = values.size();
+            Iterator<? extends Map.Entry<?, ?>> entries = values.entrySet().iterator();
+            writer.write('{');
+
+            if (length == 1) {
+                Map.Entry entry = entries.next();
+                writeString(writer, String.valueOf(entry.getKey()));
+                writer.write(':');
+                writeJsonValue(writer, entry.getValue());
+            } else if (length != 0) {
+                while (entries.hasNext()) {
+                    Map.Entry entry = entries.next();
+                    if (commanate) {
+                        writer.write(',');
+                    }
+                    writeString(writer, String.valueOf(entry.getKey()));
+                    writer.write(':');
+                    writeJsonValue(writer, entry.getValue());
+                    commanate = true;
+                }
+            }
+            writer.write('}');
+            return writer;
+        }
+
+        /**
+         * Writes a json array string from the given Iterable to the given writer.　Please note that this assumes that no
+         * data structures are cyclical, and that all iterables are finite.
+         *
+         * @param writer   The writer to write to.
+         * @param iterable The iterable to produce values to put in the json array. Of values taken from the iterable:
+         *                 All Iterables will be formatted as json arrays, all Maps will be formatted as json maps. A
+         *                 JsonException is thrown if a value which is neither Iterable, Map, Number, String, Boolean
+         *                 nor null is found.
+         * @throws JsonException If a value of an unknown type is found.
+         * @throws IOException   If the underlying writer throws an IOException.
+         */
+        public static Writer writeJsonArray(Writer writer, Iterable<?> iterable) throws JsonException, IOException {
+            boolean alreadyDeclaredValue = false;
+            writer.write('[');
+
+            for (Object obj : iterable) {
+                if (alreadyDeclaredValue) {
+                    writer.write(',');
+                }
+                writeJsonValue(writer, obj);
+                alreadyDeclaredValue = true;
+            }
+
+            writer.write(']');
+            return writer;
+        }
+
+        /**
+         * Writes a json array string from the given object to the given writer. Please note that this assumes that no
+         * data structures are cyclical, and that all iterables are finite.
+         *
+         * @param writer The writer to write to.
+         * @param value  The value to format. All Iterables will be formatted as json arrays, all Maps will be formatted
+         *               as json maps. A JsonException is thrown if value is neither Iterable, Map, Number, String,
+         *               Boolean nor null is found.
+         * @throws JsonException If value is of an unknown type, or a Map or Iterable value produces a value of an
+         *                       unkonwn type.
+         * @throws IOException   If the underlying writer throws an IOException.
+         */
+        public static Writer writeJsonValue(Writer writer, Object value) throws JsonException, IOException {
+            if (value == null) {
+                writer.write("null");
+            } else if (value instanceof Map) {
+                writeJsonObject(writer, (Map) value);
+            } else if (value instanceof Iterable) {
+                writeJsonArray(writer, (Iterable) value);
+            } else if (value instanceof Number) {
+                writeNumber(writer, (Number) value);
+            } else if (value instanceof Boolean) {
+                writer.write(value.toString());
+            } else if (value instanceof String) {
+                writeString(writer, value.toString());
+            } else {
+                throw new JsonException("Invalid value: expected null, Map, Iterable, Number, Boolean or String, found `" + value.toString() + "` (`" + value.getClass() + "`)");
+            }
+            return writer;
+        }
+
+        public static class JsonException extends Exception {
+
+            public JsonException(String message) {
+                super(message);
+            }
+        }
+    }
 }
